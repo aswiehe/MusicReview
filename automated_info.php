cc6c8a8ec219e01dc71a8fb737e712fafcf72411
gitsauron <gitsauron@music_theory_review.com>
2020-12-24 10:55:32 +0000

    This commit marks lamp_setup.sh being used to create a new project locally

---
+++ automated_info.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Git PHP Test</title>
+  </head>
+  <body>
+    <h1>
+      Test Commit File
+    </h1>
+    <h3>
+      <?php echo "This was echo'd from PHP"; ?>
+    </h3>
+    <br>
+    <?php phpinfo(); ?>
+  </body>
+</html>
