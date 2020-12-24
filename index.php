1b14bcf3146c6c8b4a879c3ee86bc979704b91e3
root <gitsauron@mtr.com>
2020-12-24 10:18:56 +0000

    new file for testing automated git setup bash script

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>PHP test</title>
+  </head>
+  <body>
+    <h1>
+      <?php echo "This was echo'd from PHP!"; ?>
+    <h1>
+    <br>
+    <?php phpinfo(); ?>
+  </body>
+</html>
