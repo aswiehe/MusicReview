a00d5fc96e0214649dbaa1cf8ed34bcc6d9134e0
gitsauron <gitsauron@mtr.com>
2020-12-24 14:29:03 +0000

    This commit indicates that the script (currently named lamp_setup.sh) being used to create a new project locally (probably hosted on a VPS) was successfully able to git push

--- automated_info.php
+++ automated_info.php
@@ -9,6 +9,9 @@
     <h3>
       <?php echo "This was echo'd from PHP"; ?>
     </h3>
+    <p>
+      The purpose of this file is to act as a sort of "marker" that a new instance of this application has been created on a local system. This file will be removed by the same script that created it, but the commit used to push it will remain as an indicator that the local system can successfully push to the repo.
+    </p>
     <br>
     <?php phpinfo(); ?>
   </body>
